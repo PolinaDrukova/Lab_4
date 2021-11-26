e72a98ae36879a9dd6b4d0d7b4cacf33e44235f6
PolinaDrukova <70808551+PolinaDrukova@users.noreply.github.com>
2021-11-26 10:58:06 +0300

    Add files via upload

---
+++ Laba_4/Laba_4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Laba_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstLevel f = new FirstLevel("vistar", 12, 2);
+            Console.WriteLine(f.getQ());
+            FirstLevel f2 = new SecondLevel("vistar", 12, 2, 3);
+            Console.WriteLine(f2.getQ());
+        }
+    }
+
+    public class SecondLevel : FirstLevel
+    {
+        public int averageSpeed { set; get; }
+        public SecondLevel(string title, int quantityWorkStations, int averageDistance, int averageSpeed)
+        : base(title, quantityWorkStations, averageDistance)
+        {
+            this.averageSpeed = averageSpeed;
+        }
+
+        public override int getQ()
+        {
+            return quantityWorkStations * averageDistance * averageSpeed;
+        }
+    }
+
+    public class FirstLevel
+    {
+        public string title { get; set; }
+        public int quantityWorkStations { get; set; }
+        public int averageDistance { get; set; }
+
+        public FirstLevel(string title, int quantityWorkStations, int averageDistance)
+        {
+            this.title = title;
+            this.quantityWorkStations = quantityWorkStations;
+            this.averageDistance = averageDistance;
+        }
+
+        public virtual int getQ()
+        {
+            return quantityWorkStations * averageDistance;
+        }
+    }
+}
